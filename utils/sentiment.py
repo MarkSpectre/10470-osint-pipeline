194cab9d98eb298f16921dfbb166efc2b168eb3c
MarkSpectre <114685111+MarkSpectre@users.noreply.github.com>
2025-09-22 00:40:09 +0530

    Update sentiment.py

--- utils/sentiment.py
+++ utils/sentiment.py
@@ -1,18 +1,3 @@
-# from textblob import TextBlob 
-
-# def add_sentiment(records): 
-#     if records is None:
-#         return []
-    
-#     for r in records:
-#         # Only add sentiment if text exists
-#         if "text" in r and r["text"] is not None and isinstance(r["text"], str):
-#             r["sentiment"] = TextBlob(r["text"]).sentiment.polarity
-#         else:
-#             r["sentiment"] = 0.0  # Default sentiment for records without text
-    
-#     return records
-
 from textblob import TextBlob
 
 def add_sentiment(data):
@@ -40,3 +25,4 @@ def add_sentiment(data):
         record["sentiment"] = sentiment
 
     return data
+
